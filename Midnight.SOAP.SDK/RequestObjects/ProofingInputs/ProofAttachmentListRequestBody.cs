8b570bb0571f22c580b39aa5f746087404cabee5
midnightmeshllc <227396731+midnightmeshllc@users.noreply.github.com>
2025-08-21 23:32:40 -0400

    fix: camelcase

--- Midnight.SOAP.SDK/RequestObjects/ProofingInputs/ProofAttachmentListRequestBody.cs
+++ Midnight.SOAP.SDK/RequestObjects/ProofingInputs/ProofAttachmentListRequestBody.cs
@@ -6,10 +6,10 @@ namespace Midnight.SOAP.SDK.RequestObjects.ProofingInputs;
 [XmlRoot("RequestAttachmentList")]
 public class ProofAttachmentListRequestBody
 {
-    public required ProofAttachmentLIstInputParameter InputParameter { get; set; }
+    public required ProofAttachmentListInputParameter InputParameter { get; set; }
 }
 
-public class ProofAttachmentLIstInputParameter
+public class ProofAttachmentListInputParameter
 {
     public required int RequestID { get; set; }
 }
