aafdd103cb9ac5ad2d184fbf07555e4d96e508ad
midnightmeshllc <227396731+midnightmeshllc@users.noreply.github.com>
2025-08-21 23:33:33 -0400

    fix: class name

--- Midnight.SOAP.SDK/RequestObjects/ProofingInputs/ProofNewRequestBody.cs
+++ Midnight.SOAP.SDK/RequestObjects/ProofingInputs/ProofNewRequestBody.cs
@@ -7,20 +7,18 @@ namespace Midnight.SOAP.SDK.RequestObjects.ProofingInputs;
 [XmlRoot("RequestNew")]
 public class ProofNewRequestBody
 {
-    public required ProofNewRequestInputParameter InputParameter { get; set; }
+    public required ProofNewInputParameter InputParameter { get; set; }
 }
 
-public class ProofNewRequestInputParameter
+public class ProofNewInputParameter
 {
     public required List<RequestNew> Requests { get; set; } = new List<RequestNew>();
 }
 
 public class RequestNew
 {
-    [Required]
-    public DateOnly RequestDate { get; set; }
-    [Required]
-    public int CustomerID { get; set; }
+    public required string RequestDate { get; set; }
+    public required int CustomerID { get; set; }
     public int? EstimateID { get; set; }
     public int? VersionID { get; set; }
     public int? OrderVersionDetailID { get; set; }
