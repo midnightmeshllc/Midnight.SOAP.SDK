54831922f6ea303a2e2c506db947ca4304a108e5
midnightmeshllc <227396731+midnightmeshllc@users.noreply.github.com>
2025-08-21 23:32:06 -0400

    fix: DateOnly to string

--- Midnight.SOAP.SDK/RequestObjects/OrderInputs/OrderShipmentNewRequestBody.cs
+++ Midnight.SOAP.SDK/RequestObjects/OrderInputs/OrderShipmentNewRequestBody.cs
@@ -8,8 +8,8 @@ public class OrderShipmentNewRequestBody
     public required int OrderID { get; set; }
     public required int VersionID { get; set; }
     public int? LocationID { get; set; }
-    public required string TrackingNo { get; set; }
-    public required DateOnly ShippedDate { get; set; }
+    public required string TrackingNo { get; set; } = string.Empty;
+    public required string ShippedDate { get; set; } = string.Empty;
     public decimal? Weight { get; set; } = decimal.Zero;
     public decimal? Cost { get; set; } = decimal.Zero;
     public string? ServiceType { get; set; }
