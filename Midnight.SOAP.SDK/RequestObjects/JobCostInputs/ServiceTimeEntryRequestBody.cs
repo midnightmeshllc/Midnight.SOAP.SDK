0fd6b20db0374a2cbcbf69716da3842f481f169c
midnightmeshllc <227396731+midnightmeshllc@users.noreply.github.com>
2025-08-21 23:31:22 -0400

    fix: required props

--- Midnight.SOAP.SDK/RequestObjects/JobCostInputs/ServiceTimeEntryRequestBody.cs
+++ Midnight.SOAP.SDK/RequestObjects/JobCostInputs/ServiceTimeEntryRequestBody.cs
@@ -6,19 +6,16 @@ namespace Midnight.SOAP.SDK.RequestObjects.JobCostInputs;
 [XmlRoot("JobCostServiceTimeEntryRequest")]
 public class ServiceTimeEntryRequestBody
 {
-    [Required]
-    public int EmployeeID { get; set; }
-    [Required]
-    public DateOnly ActivityDate { get; set; }
-    public List<ServiceTimeEntryRequestItem> JobCostItems { get; set; }
+    public required int EmployeeID { get; set; }
+    public required string ActivityDate { get; set; }
+    public required List<ServiceTimeEntryRequestItem> JobCostItems { get; set; }
 }
 
 public class ServiceTimeEntryRequestItem
 {
-    [Required]
-    public string? OperationCode { get; set; }
-    public int HelpersCount { get; set; }
-    public decimal TotalTime { get; set; }
+    public required string OperationCode { get; set; }
+    public required int HelpersCount { get; set; }
+    public required decimal TotalTime { get; set; }
     public string? JobComment { get; set; }
     public int? OrderID { get; set; }
 }
